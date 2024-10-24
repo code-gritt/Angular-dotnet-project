384bdf400e74c76fc2fb5272243a48d1829418f7
gokul <gokulchandan24@gmail.com>
2024-10-25 01:36:24 +0530

    added tailwind

--- client/contactly/src/app/app.component.ts
+++ client/contactly/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClient],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'contactly';
+  http = inject(HttpClient);
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7122/api/Contacts');
+  }
 }
