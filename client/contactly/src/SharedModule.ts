e03e0ec1e756179dce304ec04654a5e52f137b26
gokul <gokulchandan24@gmail.com>
2024-10-25 01:47:27 +0530

    received end point

---
+++ client/contactly/src/SharedModule.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  exports: [],
+  imports: [HttpClientModule],
+})
+export class SharedModule {}
