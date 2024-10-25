679a2f4ff3811ef45d8a83bd9ab6bb40d8dd8aa4
Gokul <153720121+code-gritt@users.noreply.github.com>
2024-10-25 19:44:58 +0530

    api backend code using dotnet added

---
+++ api/Contactly/Contactly/Data/ConatctlyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Contactly.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contactly.Data
+{
+    public class ConatctlyDbContext : DbContext
+    {
+        public ConatctlyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
